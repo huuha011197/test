0159349dfd3f7cf894624e71c24974ecc3a27663
tam phan <tamphan01001@gmail.com>
2025-08-05 12:17:36 +0700

    fix UI

--- components/MobileMenu.vue
+++ components/MobileMenu.vue
@@ -1,11 +1,9 @@
 <template>
     <div class="mobile-menu-container">
-        <div v-if="isOpen" @click="onClose"
-            class="mobile-menu-overlay"
+        <div v-if="isOpen" @click="onClose" class="mobile-menu-overlay"
             :class="{ 'opacity-100': isOpen, 'opacity-0': !isOpen }"></div>
 
-        <div class="mobile-menu-panel"
-            :class="{ 'translate-x-0': isOpen, 'translate-x-full': !isOpen }">
+        <div class="mobile-menu-panel" :class="{ 'translate-x-0': isOpen, 'translate-x-full': !isOpen }">
             <div class="mobile-menu-header">
                 <div class="mobile-menu-lang-dropdown-wrapper">
                     <DropdownLang :is-mobile="true" />
@@ -20,18 +18,27 @@
             </div>
 
             <nav class="mobile-menu-nav">
-                <NuxtLink v-for="link in NAVLINKS" :to="link.url" class="item">
-                    {{ link.label }}</NuxtLink>
+                <NuxtLink v-for="link in nav" :to="rt(link.url)" class="item">
+                    {{ rt(link.label) }}</NuxtLink>
             </nav>
         </div>
     </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 const props = defineProps({
     isOpen: Boolean,
     onClose: Function
 })
+interface Nav {
+    url: string;
+    label: string;
+}
+
+type NavLink = Nav[];
+const { tm, rt } = useI18n();
+const nav = tm("navLink") as NavLink;
+
 </script>
 
 <style scoped>
@@ -70,11 +77,54 @@ const props = defineProps({
 .item {
     @apply block px-4 py-3 text-black hover:bg-gray-100 hover:text-gray-900 uppercase tracking-wide text-sm font-bold w-full text-center;
 
-    border-bottom: 1px solid theme('colors.gray.200'); 
+    border-bottom: 1px solid theme('colors.gray.200');
 }
 
 .item:not(:last-child) {
-    @apply border-b border-gray-200; 
-   
+    @apply border-b border-gray-200;
+
 }
 </style>
+
+<i18n lang="json">{
+    "vi": {
+        "navLink": [
+            {
+                "label": "VỀ CHÚNG TÔI",
+                "url": "#about"
+            },
+            {
+                "label": "TRÒ CHƠI",
+                "url": "#service"
+            },
+            {
+                "label": "ĐỐI TÁC",
+                "url": "#work"
+            },
+            {
+                "label": "LIÊN HỆ",
+                "url": "#footer"
+            }
+        ]
+    },
+    "en": {
+        "navLink": [
+            {
+                "label": "ABOUT US",
+                "url": "#about"
+            },
+            {
+                "label": "GAMES",
+                "url": "#service"
+            },
+            {
+                "label": "PARTNERS",
+                "url": "#work"
+            },
+            {
+                "label": "CONTACT US",
+                "url": "#footer"
+            }
+        ]
+    }
+}</i18n>
